3c7f03298847a7c6c491352d1daa22951b8f513f
etteryand0 <etteryand@gmail.com>
2020-12-25 19:07:31 +0900

    Алгоритм проверяет, недействующая ли компания и находится ли на стадии разработки (вместо $ index массива с даннымпи. жду базу данных)

--- lib/loyalty.py
+++ lib/loyalty.py
@@ -1,3 +1,37 @@
 class Loyalty:
-    def __init__(self):
-        pass
+    def __init__(self, company):
+        self.company = company # list
+        self.score = 10000
+        self.coefficient = {
+            'inoperative': 1,
+            'liquidated': 1
+        } # коэффиценты для вычитания\прибавления баллов
+    
+    def save_score(self, score, reason):
+        return score * self.coefficient[reason]
+
+    def check_status(self):
+        if self.operative():
+            if self.not_liquidated():
+                return True
+            else:
+                self.score -= self.save_score(self.score, 'liquidated')
+                return False
+        else:
+            self.score -= self.save_score(self.score, 'inoperative')
+            return False
+
+    def operative(self):
+        # Проверяем, является ли компание действующей
+        invalid = self.company[$] # [AL]
+        if invalid == 'True':
+            return False
+
+        return True
+
+    def not_liquidated(self):
+        liquidated = self.company[$] # [AM]
+        if liquidated == 'True':
+            return False
+
+        return True
