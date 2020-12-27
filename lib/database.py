0f1280f1267ca53224524d453ea03307ca4058e1
etteryand0 <etteryand@gmail.com>
2020-12-27 12:41:39 +0900

    константы - колонки

--- lib/database.py
+++ lib/database.py
@@ -1,16 +1,40 @@
+import sqlite3
+
+
 class Database:
     def __init__(self):
         # Обозначаем нужные константы - колонки
-        pass
+        # part1 A-P
+        # B - 1
+        # D - 3
+        self.part1 = {'B': 1, 'D': 3}
+        # part3 AG-BR
+        # AL - 6  # AM - 7
+        # AR - 12
+        self.part3 = {'AL': 6, 'AM': 7, 'AR': 12}
+        # part5 BU-
+        # BV - 2  # BY - 5
+        # CE - 11
+        self.part4 = {'BV': 2, 'BY': 5, 'CE': 11}
+        # part12 -HN
+        # HE - 1  # HG - 3
+        # HH - 4  # HK - 7
+        # HL - 8
+        self.part12 = {'HE': 1, 'HG': 3, 'HH': 4, 'HK': 7, 'HL': 8}
+
+        # part13 HO-
+        #
 
     @staticmethod
-    def connect_to_db():
+    def connect_to_db(filename):
         # Открываем файл с секретами
         # Подключаемся к базе данных
-        # Возвращает массив, в которой connection и cursor
-        pass
+        # Возвращает connection
+        connection = sqlite3.connect(f'reference/database.db')
 
-    def grab_data(self, cursor):
+        return connection
+
+    def grab_data(self, connection):
         # Берём нужные данные
         # Колонки - константы, их берём с self
         # Возвращает константу с данными data
